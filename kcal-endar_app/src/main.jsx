60c0969fb69103451905073026a0765475deed32
Mrcgllmbrd <m.giallombardo91@gmail.com>
2023-12-09 20:39:17 +0100

    feat: added login custom hook

--- kcal-endar_app/src/main.jsx
+++ kcal-endar_app/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import  App  from './app'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <App />
   </React.StrictMode>,
 )
