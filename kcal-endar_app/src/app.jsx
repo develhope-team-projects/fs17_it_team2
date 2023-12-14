05668754dd02a04cb8c2d4faf6217524712dae40
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2023-12-14 19:25:00 +0100

    feat: navbar component created

--- kcal-endar_app/src/app.jsx
+++ kcal-endar_app/src/app.jsx
@@ -1,9 +1,10 @@
 import { LoginSignup } from "./login&signup_page/components/LoginSignup";
+import { NavbarSite } from "./navbar/NavbarSite";
 
 export const App = () => {
   return (
     <div>
-      <div>{/* navbar */}</div>
+      <div><NavbarSite/></div>
       {/* Componenti */}
       <LoginSignup />
       <div>{/* footer */}</div>
---
+++ kcal-endar_app/src/navbar/NavbarSite.jsx
@@ -0,0 +1,52 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../navbar/navbarStyle-style.css'
+import logo from '../-assets/logo/Logo.name.png';
+import '../-common/colors.css';
+import '../-common/font.css';
+import avatar from '../-assets/icons/user-avatar-light-blue.svg';
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive'
+
+export function NavbarSite() {
+
+    const [navCollapse, setNavCollapse] = useState(false);
+
+    const isVisible = useMediaQuery({ query: '(max-width: 1279px)' });
+
+    useEffect(() => {
+        setNavCollapse(isVisible);
+    }, [isVisible])
+
+    return (
+        <Navbar bsPrefix='navbar' expand="lg" id='navbar-style'>
+            <Container id='navbar-container'>
+                <Navbar.Brand href="#home" id='navbar-brand-logo'>
+                    <img src={logo} alt='KCALendar' id='navbar-logo' />
+                </Navbar.Brand>
+                <Navbar.Brand id='navbar-brand-visible'>
+                    <Nav.Link href="#chi-siamo">Chi siamo</Nav.Link>
+                    <Nav.Link href="#come-funziona">Come funziona</Nav.Link>
+                    <Nav.Link href="#professionisti">I nostri professionisti</Nav.Link>
+                    <Nav.Link href="#ricette">Ricette</Nav.Link>
+                </Navbar.Brand>
+                {navCollapse &&
+                    <Navbar.Collapse id="navbar-collapse">
+                        <Nav>
+                            <Nav.Link href="#chi-siamo">Chi siamo</Nav.Link>
+                            <Nav.Link href="#come-funziona">Come funziona</Nav.Link>
+                            <Nav.Link href="#professionisti">I nostri professionisti</Nav.Link>
+                            <Nav.Link href="#ricette">Ricette</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>}
+                <Navbar.Brand id='navbar-brand-avatar'>
+                    <Nav.Link href="#profile">
+                        <img src={avatar} width="40" height="40" />
+                    </Nav.Link>
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
+}
