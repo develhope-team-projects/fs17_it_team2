9ad400fb03d671400896bc52152e0b6d5efa4a00
giovaspat <141187369+giovaspat@users.noreply.github.com>
2023-12-15 18:32:48 +0100

    feat/footer

--- kcal-endar_app/src/app.jsx
+++ kcal-endar_app/src/app.jsx
@@ -2,6 +2,7 @@ import "../src/-common/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Login } from "./login&signup_page/components/Login";
 import { Signup } from "./login&signup_page/components/Signup";
+import { Footer } from "./footer/footer";
 
 
 export const App = () => {
@@ -12,6 +13,7 @@ export const App = () => {
       <Login />
       <Signup />
       <div>{/* footer */}</div>
+      <Footer />
     </div>
   );
 };
