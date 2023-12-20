3dc942a683223cba171f5b8a42377d5df9b134ef
giovaspat <141187369+giovaspat@users.noreply.github.com>
2023-12-20 19:27:54 +0100

    feat landing page step cards and blog

---
+++ kcal-endar_app/src/landingPage_StepsAndBlog/ButtonRegistrati.jsx
@@ -0,0 +1,13 @@
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./ButtonRegistrati.css";
+
+export function ButtonRegistrati() {
+  return (
+    <div className="button-container">
+      <Button href="#" type="submit" className="button-container-registrati">
+        Registrati
+      </Button>
+    </div>
+  );
+}
