5cb945c47c24682f220c3a55aaa0e68257a6d741
giovaspat <141187369+giovaspat@users.noreply.github.com>
2024-01-23 01:11:15 +0100

    fixed routes and css recipe pages

--- kcal-endar_app/src/ricette_component/Ricette.jsx
+++ kcal-endar_app/src/ricette_component/Ricette.jsx
@@ -1,13 +1,12 @@
-
+import { Link } from "react-router-dom";
 import { AllRecipes } from "../ricette_component/Components/AllRecipes";
 
 export function Ricette() {
-
   return (
     <div>
-      <h1>RICETTE</h1>
-      {/* <Link to="/">Ritorna alla Home</Link> */}
+      <Link to="/ricette">
+      </Link>
       <AllRecipes />
     </div>
-  )
+  );
 }
