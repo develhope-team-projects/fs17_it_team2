71fd1fac18aded5e8d3e3f26cc66ac48d231cb79
Mrcgllmbrd <m.giallombardo91@gmail.com>
2024-01-24 18:51:49 +0100

    fixed backend

--- kcal-endar_app/src/doctor-dashboard_component/DoctorDashboard.jsx
+++ kcal-endar_app/src/doctor-dashboard_component/DoctorDashboard.jsx
@@ -1,10 +1,12 @@
 import { ContainerSchedeDoc } from "./components/ContainerSchedeDoc";
+import {UserDashboardCalendar} from "../user-dashboard_component/Components/UserDashboardCalendar"
 import "./style/DoctorDashboard.css";
 
 export const DoctorDashboard = () => {
   return (
     <div className="doctor-dashboard-container">
       <ContainerSchedeDoc />
+      <UserDashboardCalendar />
     </div>
   );
 };
