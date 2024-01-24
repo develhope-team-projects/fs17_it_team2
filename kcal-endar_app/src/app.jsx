4f4907bb5b8c1248b102b17628eceaad1ad87a70
Marta <marta.ferraris25@gmail.com>
2024-01-24 02:08:33 +0100

    feature: add i nostri professionisti section and it's css

--- kcal-endar_app/src/app.jsx
+++ kcal-endar_app/src/app.jsx
@@ -10,6 +10,7 @@ import { Signup } from "./signup_component/SignUp";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ChiSiamo } from "./chi_siamo_section/ChiSiamo";
 import { UserProvider } from "./-shared/UserContext";
+import { ProfSection } from "./professionisti_section/ProfSection";
 
 export const App = () => {
   return (
@@ -24,7 +25,8 @@ export const App = () => {
             <Route path="/signup" element={<Signup />} />
             <Route path="/user-dashboard" element={<UserDashboard />} />
             <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
-          <Route path="/chi-siamo" element={<ChiSiamo />} />
+            <Route path="/chi-siamo" element={<ChiSiamo />} />
+            <Route path="/professionisti" element={<ProfSection/>}/>
             <Route path="/ricette" element={<Ricette />} />
           </Routes>
         </BrowserRouter>
